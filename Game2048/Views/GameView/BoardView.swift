fe85150e9867e5a82640a49903ea95a0f4dbd8ad
Charlie Parker <charlienparker@gmail.com>
2024-01-19 23:50:07 -0500

    breaks out game screen views into their own classes

---
+++ Game2048/Views/GameView/BoardView.swift
@@ -0,0 +1,8 @@
+//
+//  BoardView.swift
+//  Game2048
+//
+//  Created by user253524 on 1/19/24.
+//
+
+import Foundation
---
+++ Game2048/Views/GameView/LeaderboardButton.swift
@@ -0,0 +1,8 @@
+//
+//  LeaderboardButton.swift
+//  Game2048
+//
+//  Created by user253524 on 1/19/24.
+//
+
+import Foundation
