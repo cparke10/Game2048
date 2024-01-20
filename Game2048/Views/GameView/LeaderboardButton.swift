33cb65599ffea9a8b591563c8dc8ae71f1734c79
Charlie Parker <charlienparker@gmail.com>
2024-01-20 00:41:50 -0500

    adds missing leaderboard diff

--- Game2048/Views/GameView/LeaderboardButton.swift
+++ Game2048/Views/GameView/LeaderboardButton.swift
@@ -5,4 +5,33 @@
 //  Created by user253524 on 1/19/24.
 //
 
-import Foundation
+import SwiftUI
+
+/// The view used to represent the leaderboard navigation button.
+struct LeaderboardButton: View {
+    
+    private static let backgroundColor = Color(red: 211/255, green: 211/255, blue: 211/255)
+    private static let buttonTitleString = NSLocalizedString("Leaderboard", comment: "Leaderboard navigation button content")
+    
+    var body: some View {
+        ZStack {
+            HStack {
+                NavigationLink(destination: LandingPage()) {
+                    Text(Self.buttonTitleString)
+                        .font(.system(size: 18))
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(Color.blue, lineWidth: 2)
+                                .fill(Color.white)
+                        )
+                }
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+        .overlay(Divider().background(Color.blue), alignment: .top)
+        .background(Self.backgroundColor)
+    }
+}
