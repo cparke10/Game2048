6d030e6d54fb864b57ef9d0a6aa9a04f34617b42
Charlie Parker <charlienparker@gmail.com>
2024-01-26 15:46:23 -0500

    updates leaderboard call to send id in param and remove id from body

--- Client/Game2048/Source/Leaderboard/LeaderboardService.swift
+++ Client/Game2048/Source/Leaderboard/LeaderboardService.swift
@@ -9,15 +9,19 @@ import Foundation
 
 /// Manges requests to the leaderboard API.
 class LeaderboardService {
-    private let baseURL = "https://www.google.com/" // TODO: replace with LeaderboardServer url
-    private var userId: String? { UserManager.shared.user?.id }
+    private let baseURLString = "https://www.google.com/" // TODO: replace with LeaderboardServer url
+    private var userURLString: String? {
+        guard let userId = UserManager.shared.user?.id else { return nil }
+        
+        return "\(baseURLString)/\(userId)"
+    }
     
     /// Performs the leaderboard API request for the list of entries.
     /// - Parameters:
     ///   - type: The `LeaderboardType` to request the API for.
     ///   - completionHandler: The completion block to handle the service response.
     func requestLeaderboard(for type: LeaderboardType, completionHandler: @escaping (Result<LeaderboardResponse, Error>) -> Void) {
-        guard let userId, let url = URL(string: "\(baseURL)\(type == .me ? userId : "")") else { return }
+        guard let urlString = type == .me ? userURLString : baseURLString, let url = URL(string: urlString) else { return }
         
         URLRequest.request(URLRequest(url: url), responseType: LeaderboardResponse.self, completionHandler: completionHandler)
     }
@@ -27,10 +31,7 @@ class LeaderboardService {
     ///   - score: The game score to upload to the API.
     ///   - completionHandler: The completion block to handle the service response.
     func submitEntry(score: Int, completionHandler: @escaping (Result<SubmissionResponse, Error>) -> Void) {
-        guard let userId, let url = URL(string: baseURL) else { return }
-        
-        let entry = LocalLeaderboardEntry(id: userId, score: score)
-        guard let body = try? JSONEncoder().encode(entry) else { return }
+        guard let url = URL(string: baseURLString), let body = try? JSONEncoder().encode(LocalLeaderboardEntry(score: score)) else { return }
         
         var submitRequest = URLRequest(url: url)
         submitRequest.httpMethod = URLRequest.HTTPMethod.put.rawValue
